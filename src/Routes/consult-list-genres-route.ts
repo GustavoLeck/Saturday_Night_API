467802a3904949616785424bc48066cd24abf9d4
-v <gustavoleck@hotmail.com>
2023-05-01 17:46:24 -0300

    feat(Criado rotas de consulta de filmes e generos)

---
+++ src/Routes/consult-list-genres-route.ts
@@ -0,0 +1,12 @@
+
+import { Router, Request, Response } from 'express';
+
+import { ConsultListGenresController } from "../Controller/consult-list-genres-controller";
+
+const router = Router();
+
+
+
+router.get('/genres', new ConsultListGenresController().handle)
+
+export default router;
