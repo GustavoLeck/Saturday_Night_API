d006e91ae1b01e6205de3b9217c23dc626f10f9a
-v <gustavoleck@hotmail.com>
2023-05-01 17:46:19 -0300

    feat(Criado rotas de consulta de filmes e generos)

---
+++ src/Routes/consult-movie-route.ts
@@ -0,0 +1,12 @@
+
+import { Router, Request, Response } from 'express';
+
+import { ConsultMovieController } from "../Controller/consult-movie-controller";
+
+const router = Router();
+
+
+
+router.get('/movie/:ano/:generos', new ConsultMovieController().handle)
+
+export default router;
