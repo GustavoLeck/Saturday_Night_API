8b33400fd71b3cb8bdddbff74da72ce1761dbd84
-v <gustavoleck@hotmail.com>
2023-05-01 17:47:10 -0300

    feat(Criado rota de consulta de filmes e generos)

---
+++ src/Controller/consult-list-genres-controller.ts
@@ -0,0 +1,11 @@
+import {Request, Response } from 'express';
+import { ConsultListGenres } from "../Use-Case/consult-list-genres";
+
+
+export class ConsultListGenresController{
+    async handle(req : Request, res : Response){
+        const response = await new ConsultListGenres().execute();
+        res.status(response.Code).send(response);
+
+    }
+}
