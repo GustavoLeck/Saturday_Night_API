e4a05fd891474efb056a4e91fccbabad3cd53e10
-v <gustavoleck@hotmail.com>
2023-05-01 17:46:29 -0300

    feat(Criado rotas de consulta de filmes e generos)

---
+++ src/Controller/consult-movie-controller.ts
@@ -0,0 +1,11 @@
+import {Request, Response } from 'express';
+import { ConsultMovieModel } from "../Model/consult-movie-model";
+import { ConsultMovie } from "../Use-Case/consult-movie";
+
+export class ConsultMovieController{
+    async handle(req : Request, res : Response){
+        const filtro = new ConsultMovieModel(parseInt(req.params.ano), req.params.generos.split(','))
+        const respostaPesquisaMovie = await new ConsultMovie().execute(filtro);
+        res.status(200).send(respostaPesquisaMovie);
+    }
+}
