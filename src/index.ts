b4cb4232ed83c650b832e0fb4552c1ecc574fdfc
-v <gustavoleck@hotmail.com>
2023-05-01 17:45:02 -0300

    feat(Criado rotas de consulta de filmes e generos)

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+
+//Import de rotas
+
+import Status from "./Routes/status-route";
+import Movies from "./Routes/consult-movie-route";
+import Generos from "./Routes/consult-list-genres-route";
+
+
+const app = express();
+const port = 3080;
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(cors());
+app.use(helmet());
+
+// Rotas
+app.use("/api", Status);
+app.use("/api", Movies);
+app.use("/api", Generos);
+
+// Inicialização do servidor
+app.listen(port, () => {
+  console.log(`--Server ON--`);
+});
