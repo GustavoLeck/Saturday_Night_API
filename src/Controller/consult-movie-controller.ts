c8af58414f0ba81b9efbcdb6f0296b380dcf6ecb
Gustavo Leck <gustavoleck@hotmail.com>
2023-05-02 17:12:30 -0300

    feat(Criado mensagem de log)

--- src/Controller/consult-movie-controller.ts
+++ src/Controller/consult-movie-controller.ts
@@ -4,7 +4,7 @@ import { ConsultMovie } from "../Use-Case/consult-movie";
 
 export class ConsultMovieController{
     async handle(req : Request, res : Response){
-        console.log("   => Rotas de consulta de filmess utilizada.")
+        console.log("   => Rotas de consulta de filmes utilizada")
         const filtro = new ConsultMovieModel(parseInt(req.params.ano), req.params.generos.split(','))
         const respostaPesquisaMovie = await new ConsultMovie().execute(filtro);
         res.status(200).send(respostaPesquisaMovie);
