eec7cd95a8e5ebd86296dca4492e18dd2050eae6
Gustavo Leck <gustavoleck@hotmail.com>
2023-05-02 17:09:12 -0300

    feat(Criado mensagem de log)

--- src/Routes/status-route.ts
+++ src/Routes/status-route.ts
@@ -1,9 +1,9 @@
 import express from "express"
-
+import {Request, Response } from 'express';
 const router = express.Router();
 
-router.get(`/status`, (req, res)=>{
-    console.log("Teste")
+router.get(`/status`, (req : Request, res : Response)=>{
+    console.log("   => Rotas de consulta status utilizada.")
     res.send({status: "Ok"})
 })
 
