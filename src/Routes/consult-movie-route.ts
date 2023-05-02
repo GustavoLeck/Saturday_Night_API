36c5f2ee65bf171f1c45a93a1248aba1adb28002
Gustavo Leck <gustavoleck@hotmail.com>
2023-05-02 17:09:07 -0300

    feat(Criado mensagem de log)

--- src/Routes/consult-movie-route.ts
+++ src/Routes/consult-movie-route.ts
@@ -5,8 +5,6 @@ import { ConsultMovieController } from "../Controller/consult-movie-controller";
 
 const router = Router();
 
-
-
 router.get('/movie/:ano/:generos', new ConsultMovieController().handle)
 
 export default router;
