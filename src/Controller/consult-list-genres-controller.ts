6f0862ce8633344d8a8ef9aa3e10873ca37e612f
Gustavo Leck <gustavoleck@hotmail.com>
2023-05-02 17:08:15 -0300

    feat(Criado mensagem de log)

--- src/Controller/consult-list-genres-controller.ts
+++ src/Controller/consult-list-genres-controller.ts
@@ -4,6 +4,7 @@ import { ConsultListGenres } from "../Use-Case/consult-list-genres";
 
 export class ConsultListGenresController{
     async handle(req : Request, res : Response){
+        console.log("   => Rotas de consulta de generos utilizada.")
         const response = await new ConsultListGenres().execute();
         res.status(response.Code).send(response);
     }
